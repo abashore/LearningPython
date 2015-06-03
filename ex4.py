813ef3bde31bf085639ea08a2ecb8c49a7058c37
abashore <abashore@gmail.com>
2015-06-03 01:52:57 -0400

    Finished lesson 4

--- ex4.py
+++ ex4.py
@@ -1,12 +1,27 @@
 __author__ = 'Adam'
 
+# integer variable
 cars = 100
+
+# floating point variable
 space_in_a_car = 4.0
+
+# integer variable
 drivers = 30
+
+# integer variable
 passengers = 90
+
+# simple math subtracting drivers from the total number of cars
 cars_not_driven = cars - drivers
+
+# storing information about which cars will have drivers
 cars_driven = drivers
+
+# total number of cars with drivers multiplied by seats in each car.
 carpool_capacity = cars_driven * space_in_a_car
+
+# this is dividing all the passengers by the number of cars that have drivers.
 average_passengers_per_car = passengers / cars_driven
 
 
